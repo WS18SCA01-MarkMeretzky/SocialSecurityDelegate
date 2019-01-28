3fcd9477fb75707ef03a3912784a126a899996cd
Mark Meretzky <mark.meretzky@gmail.com>
2019-01-27 20:10:28 -0500

    Initial Commit

---
+++ SocialSecurityDelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialSecurityDelegate
+//
+//  Created by Mark Meretzky on 1/27/19.
+//  Copyright © 2019 New York University School of Professional Studies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
